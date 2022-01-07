30361d028783a44e2526e8b18c4d3823de8cf176
[mswain430] <[millie.swain@gmail.com]>
2022-01-07 09:45:31 -0800

    currentdate specific js

---
+++ js/currentdate.js
@@ -0,0 +1,5 @@
+//ToLocaleDateString
+const options = {weekday: 'long' , day: 'numeric', month: 'long', year: 'numeric'};
+const year = {year: 'numeric'}
+document.getElementById('year').textContent = new Date().toLocaleDateString('en-us', year);
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-us', options);
