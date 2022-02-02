6a4d36e8a02526f6e7f5fb9e2ab289a46e4ea074
mswain430 <millie.swain@gmail.com>
2022-02-01 23:08:57 -0800

    stage

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,18 +1,17 @@
  function toggleMenu() {
-
-   document.getElementById("navigation").classList.toggle("open");
+   document.getElementById("primaryNav").classList.toggle("open");
 }
 
 const x = document.getElementById("ham");
 x.onclick = toggleMenu;
 
 
-const hambutton = document.querySelector('ham');
-const mainnav = document.querySelector('navigation');
+//const hambutton = document.querySelector('ham');
+//const mainnav = document.querySelector('navigation');
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+//hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // Solve the mid resizing issue
-window.onresize = () => {if(window.innerWidth > 760) mainnav.classList.remove('responsive')};
+//window.onresize = () => {if(window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 
