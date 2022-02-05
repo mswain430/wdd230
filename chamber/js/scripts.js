cf2d8bbe182b688ae6f77c42577a370f9ee56032
mswain430 <millie.swain@gmail.com>
2022-02-04 23:36:10 -0800

    js

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,5 +1,6 @@
  function toggleMenu() {
    document.getElementById("primaryNav").classList.toggle("open");
+   document.getElementById("ham").classList.toggle("open");
 }
 
 const x = document.getElementById("ham");
