fc95351eb14ac9658b3a1c03f57269f00cdea0bd
mswain430 <millie.swain@gmail.com>
2022-02-19 00:57:24 -0800

    windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,18 +1,10 @@
+const t = parseFloat(document.getElementById('temp').textContent);
+const s = parseFloat(document.getElementById('speed').textContent);
 
-//Math.pow(x, y)
-// t = temp  s = wind
-
-let t = parseFloat(document.querySelector("#t").textContent);
-let s = parseFloat(document.querySelector("#s").textContent);
-let windchill = 35.74 + (0.6215*t) - ((35.75 * t)*(math.power(s^0.16))) + ((0.4275*t) * (math.power(s^0.16)));
 // temp needs to be less than 50 and wind needs to be greater than 3
-if (t <= 50 && s > 3){
-    windchill = windchill(t,s);
+if(t <= 50 && s > 3){
+    const windchill = 35.74 + (0.6215 * t) - 35.75 * Math.pow(s, 0.16) + (0.4275 * t) * Math.pow(s, 0.16);
+    document.querySelector('#chill').innerHTML = `${Math.round(windchill).toFixed(2)}`;
 } else {
-    windchill = "N/A";
+    document.querySelector('#chill').innerHTML = 'N/A';
 }
-//output
- document.querySelector("#chill").textContent = windchill();
- function wc(temp, speed){
-     return "success";
- }
