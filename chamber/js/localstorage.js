66331b91fcb01ce5bedb6ccc3c22c163fb1344f8
mswain430 <millie.swain@gmail.com>
2022-02-26 23:22:49 -0800

    fixes

--- chamber/js/localstorage.js
+++ chamber/js/localstorage.js
@@ -1,5 +1,5 @@
 // initialize display elements
-const todayDisplay = document.querySelector(".today");
+//const todayDisplay = document.querySelector(".today");
 const visitDisplay = document.querySelector(".visits");
 //get the stored value in localstorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
