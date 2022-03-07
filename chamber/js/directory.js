e324a298749f0830069d1f709975ce35f476c022
mswain430 <millie.swain@gmail.com>
2022-03-06 20:24:27 -0800

    dir

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -15,15 +15,16 @@ fetch(requestURL)
 
 function displayBusiness(localbusiness) {
   // Create elements to add to the document
-  let card = document.createElement('section');
-  card.setAttribute('class', 'card');
+  let bizcard = document.createElement('section');
+  bizcard.setAttribute('class', 'bizcard');
   let logo = document.createElement('img');
-  let h2 = document.createElement('h2');
+  let h3 = document.createElement('h3');
   let addr = document.createElement('p');
   let phone = document.createElement('p');
   let web = document.createElement('p');
   let mem = document.createElement('p');
   let ordersuffix = ``;
+
 
   if(localbusiness.order === 1){
       localbusiness.order = `${localbusiness.order}`;
@@ -66,24 +67,24 @@ function displayBusiness(localbusiness) {
   logo.setAttribute('class', 'logo');
   logo.setAttribute('loading', 'lazy');
 
-  // Change the textContent property of the h2 element to contain the business' info
-  h2.innerHTML = `${localbusiness.name}`;
+  // Change the innerHTML property of the h3 element to contain the business' info
+  h3.innerHTML = `${localbusiness.name}`;
   addr.innerHTML = `${localbusiness.address}`;
   phone.innerHTML = `${localbusiness.phone}`;
-  web.innerHTML = ` ${localbusiness.website} `;
-  mem.innerHTML = `${localbusiness.membership}`;
+  web.innerHTML = `<a href="${localbusiness.website}">website</a>`;
+  mem.innerHTML = `${localbusiness.membership} sponsor`;
 
   // Add/append the section(card) with the h2 element
 
-  card.appendChild(h2);
-  card.appendChild(logo);
-  card.appendChild(addr);
-  card.appendChild(phone);
-  card.appendChild(web);
-  card.appendChild(mem);
+  bizcard.appendChild(h3);
+  bizcard.appendChild(logo);
+  bizcard.appendChild(addr);
+  bizcard.appendChild(phone);
+  bizcard.appendChild(web);
+  bizcard.appendChild(mem);
 
 
   // Add/append the existing HTML div with the cards class with the section(card)
-  biz_cards.appendChild(card);
+  biz_cards.appendChild(bizcard);
   //biz_list.appendChild(table);
 }
