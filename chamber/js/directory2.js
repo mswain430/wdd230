fca0c6ea24b60a52e229822b01e30fb733f98237
mswain430 <millie.swain@gmail.com>
2022-03-09 15:44:09 -0800

    dir

--- chamber/js/directory2.js
+++ chamber/js/directory2.js
@@ -1,6 +1,7 @@
 const requestURL = 'https://mswain430.github.io/wdd230/chamber/data/data.json';
 
 const bizDir = document.querySelector('#bizDir');
+const bizlist = document.querySelector('.bizbody');
 
  fetch(requestURL)
   .then(function (response) {
@@ -10,6 +11,7 @@ const bizDir = document.querySelector('#bizDir');
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const localbusiness = jsonObject['localbusiness'];
     localbusiness.forEach(displayBusiness);
+   /* localbusiness.forEach(displayBusinessTable); */
 });
 
 function displayBusiness(localbusiness) {
@@ -21,7 +23,6 @@ function displayBusiness(localbusiness) {
   let addr2 = document.createElement('p');
   let phone = document.createElement('p');
   let web = document.createElement('p');
-  let contact = document.createElement('p');
   let mem = document.createElement('p');
   let ordersuffix = ``;
 
@@ -36,10 +37,9 @@ function displayBusiness(localbusiness) {
   // Change the innerHTML property of the h3 element to contain the business' info
   h3.innerHTML = `${localbusiness.bizname}`;
   phone.innerHTML = `${localbusiness.phone}`;
-  addr.innerHTML = `${localbusiness.address} <span> ${localbusiness.city} ${localbusiness.zip}</span>`;
+  addr.innerHTML = `${localbusiness.address} ${localbusiness.city} ${localbusiness.zip}`;
   addr2.innerHTML = `${localbusiness.address} <br> ${localbusiness.city} ${localbusiness.zip}`;
-  web.innerHTML = `<a href="${localbusiness.website}">${localbusiness.website}</a>`;
-  contact.innerHTML = `<a href="mailto:${localbusiness.contact}">${localbusiness.contact}</a>`;
+  web.innerHTML = `<a href="${localbusiness.website}">website</a>`;
   mem.innerHTML = `${localbusiness.membership} sponsor`;
 
   // Add/append the section(card) with the h2 element
@@ -50,18 +50,30 @@ function displayBusiness(localbusiness) {
   bizcard.appendChild(addr);
   bizcard.appendChild(addr2);
   bizcard.appendChild(web);
-  bizcard.appendChild(contact);
-  bizcard.appendChild(mem);
 
 
   // Add/append the existing HTML div with the cards class with the section(card)
   bizDir.prepend(bizcard);
 }
 
-getDirectory();
-  async function getDirectory() {
-    console.log ("get data"); //temp
-    const response = await fetch(requestData);
-    const bizDir = await response.json();
-    bizDir.forEach(displayBusiness)
-  }
+function displayBusinessTable(localbusiness) {
+    let row = document.createElement('tr');
+    let bizname = document.createElement('td');
+    let addr = document.createElement('td');
+    let phone = document.createElement('td');
+    let web = document.createElement('td');
+
+
+    bizname.innerHTML = `${localbusiness.bizname}`;
+    addr.innerHTML = `${localbusiness.address}`;
+    phone.innerHTML = `${localbusiness.phone}`;
+    web.innerHTML = `<a href="${localbusiness.website}">website</a>`;
+
+    row.appendChild(bizname);
+    row.appendChild(addr);
+    row.appendChild(phone);
+    row.appendChild(web);
+
+    bizlist.appendChild(row)
+}
+
--- chamber/js/toggleviewer.js
+++ chamber/js/toggleviewer.js
@@ -1,21 +1,22 @@
-function toggleMenu() {
-    document.getElementById('#gridviewBtn').classList.toggle("open");
-    document.getElementById('#listviewBtn').classList.toggle("open");
-    document.getElementById('#bizDir').classList.toggle("open");
-}
-const lv = document.querySelector("#listviewBtn");
-    lv.onclick = toggleMenu;
-const gv = document.querySelector("#gridviewBtn");
-    gv.onclick = toggleMenu;
-
-
 let viewtype = "O";
 let ww = window.innerWidth;
 
     if (ww >= 592 && ww <= 991) {
-        toggleMenu();
+        toggleView();
         viewtype = "M";
     }
+
+function toggleView() {
+    document.getElementById('gridviewBtn').classList.toggle("open");
+    document.getElementById('listviewBtn').classList.toggle("open");
+    document.getElementById('bizDir').classList.toggle("open");
+}
+
+const lv = document.querySelector("#listviewBtn");
+    lv.onclick = toggleView();
+const gv = document.querySelector("#gridviewBtn");
+    gv.onclick = toggleView();
+
 
 function checkWidth() {
     ww = window.innerWidth;
@@ -26,8 +27,9 @@ function checkWidth() {
        }
     } else {
         if (viewtype == "M") {
-            toggleMenu();
+            toggleView();
             viewtype = "O"
         }
     }
 }
+
