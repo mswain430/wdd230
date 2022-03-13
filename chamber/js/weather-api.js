3cb4dd6ba9d93c36d74657d3cbbbfdc71923abb6
mswain430 <millie.swain@gmail.com>
2022-03-12 23:37:44 -0800

    stage

--- chamber/js/weather-api.js
+++ chamber/js/weather-api.js
@@ -3,14 +3,16 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    document.querySelector('#windspeed').textContent = jsObject.wind.speed;
 const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;
-document.querySelector('#current-temp').textContent = `${jsObject.main.temp}&#8457`;
+document.querySelector('#current-temp').textContent =`${jsObject.main.temp}`;
 document.querySelector('#icon-src').textContent = iconsrc;
 document.querySelector('#weathericon').setAttribute('src', iconsrc);
 document.querySelector('#weathericon').setAttribute('alt', desc);
 document.querySelector('figcaption').textContent = desc;
-document.querySelector('.humidity').innerHTML = `Humidity: ${jsObject.main.humidity}`;
+document.querySelector('.humidity').innerHTML = `Humidity: ${jsObject.main.humidity}&percnt;`;
 document.querySelector('#windspeed').textContent = `Windspeed: ${jsObject.wind.speed} mph`;
 
 const temp = jsObject.main.temp;
