779930ea2ee36dd1f76d3dfee9b8702579b03066
[mswain430] <[millie.swain@gmail.com]>
2022-03-16 13:31:32 -0700

    weather

--- lesson10a/js/weather-api.js
+++ lesson10a/js/weather-api.js
@@ -3,8 +3,9 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.querySelector('#current-temp').textContent = jsObject.main.temp;
-const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(1);
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
 const desc = jsObject.weather[0].description;
 document.querySelector('#icon-src').textContent = iconsrc;
 document.querySelector('#weathericon').setAttribute('src', iconsrc);
